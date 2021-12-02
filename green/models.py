3c19e67e3d93ccf3f3a962c1796d7a8c473e0fa5
ferna041 <fer.shalgadom@gmail.com>
2021-12-02 01:11:36 -0300

    funcionando

---
+++ green/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+import uuid
+
+
+class Post(models.Model):
+    identifier = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    slug = models.SlugField(max_length=20)
+    description = models.TextField()
+    image = models.ImageField(
+        upload_to="images/",
+        default="images/Fondo.png"
+    )
+    published = models.DateTimeField(default=timezone.now)
+    likes = models.IntegerField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        related_name="green_post"
+    )
+
+    class Meta:
+        ordering = ("-published",)
+
+    def __str__(self):
+        return self.description
---
+++ green/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Post
+
+def home(request):
+    data = Post.objects.all()
+    return render(request, "home.html", {"posts": data})
